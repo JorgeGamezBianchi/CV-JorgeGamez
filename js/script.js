2b738a7ba13c6fcaa414f6d05616527250e2e9aa
JorgeGamez <jorge.gamez.mx@gmail.com>
2025-04-22 17:43:52 -0600

    Second commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const aboutBtns = document.querySelectorAll('.about-btn');
+
+aboutBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        const aboutDetail = document.querySelectorAll('.about-detail');
+
+        aboutBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+
+        aboutDetail.forEach(detail => {
+            detail.classList.remove('active');
+        });
+        aboutDetail[idx].classList.add('active');
+    });
+});
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    imgSlide.style.transform = 'translateX(calc(${index * -100}% - ${index * 2}rem))';
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < 4) {
+        index++;
+    }
+    else {
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+    activePortfolio();
+})
+
+arrowLeft.addEventListener('click', () => {
+    if (index > 1) {
+        index--;
+    }
+    else {
+        index = 0;
+    }
+    activePortfolio();
+})
