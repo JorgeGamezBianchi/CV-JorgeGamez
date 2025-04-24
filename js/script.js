ea9eac382f4bd2c268aab7874cdf1ed3edee6c30
JorgeGamez <jorge.gamez.mx@gmail.com>
2025-04-23 18:11:49 -0600

    "Commit 23/04/2025"

--- js/script.js
+++ js/script.js
@@ -22,7 +22,14 @@ const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left
 let index = 0;
 const activePortfolio = () => {
     const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+
     imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail =>{
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active');
 }
 
 arrowRight.addEventListener('click', () => {
@@ -48,3 +55,31 @@ arrowLeft.addEventListener('click', () => {
     }
     activePortfolio();
 })
+
+const form = document.querySelector("form");
+const name = document.getElementById("name");
+const mail = document.getElementById("mail");
+const phon = document.getElementById("number");
+const subj = document.getElementById("subject");
+const mssg = document.getElementById("message");
+function sendEmail() {
+
+    const myEmail = 'jorge.gamez.mx@gmail.com';
+    const bodyMsg = `Name: ${name.value}<br>Email: ${mail.value}<br>Phone: ${phon.value}<br> Message: ${mssg.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "jorge.gamez.mx@gmail.com",
+        Password : "82A689FE25F3740FEF6FE0209D4B9B376FB6",
+        To : myEmail,
+        From : myEmail,
+        Subject : subj.value,
+        Body : bodyMsg
+    }).then(
+      message => alert(message)
+    );
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
